09c46a70996923a3f3ca841b9ab7e8904bc85bf0
Yanko Rashkov <thewhitedead@abv.bg>
2023-11-20 22:04:12 +0200

    test

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import * as api from './api.js';
 
-const host = 'http://localhost:3030'
-api.settings.host = 'http://localhost:3030';
+const host = 'http://0.0.0.0:3030'
+api.settings.host = 'http://0.0.0.0:3030';
 
 export const login = api.login;
 export const register = api.register;

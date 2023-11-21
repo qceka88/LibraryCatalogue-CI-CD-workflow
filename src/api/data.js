ec914e326d8cc54634cfa5d20327349831d8bbd9
Yanko Rashkov <thewhitedead@abv.bg>
2023-11-21 21:52:03 +0200

    Update data.js

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import * as api from './api.js';
 
-const host = 'http://localhost:3030'
-api.settings.host = 'http://localhost:3030';
+const host = 'https://workflow-exercise2-ci-cd.onrender.com/:3030'
+api.settings.host = 'https://workflow-exercise2-ci-cd.onrender.com/:3030';
 
 export const login = api.login;
 export const register = api.register;
